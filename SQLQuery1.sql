884f4c8732e900cd068ce71bf4405d23cc5911e3
FlorentBunjaku1 <fb48234@ubt-uni.net>
2021-01-02 15:33:02 +0100

    Krijimi i Tabeles Selia

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -14,3 +14,16 @@ Create Table DrejtoriEkzekutiv(
 	ZipKodi int Not Null,
 	Zyrja int Foregin Key Zyrja(Nr_Dhomes) ON Update Cascade ON Delete Set Null
 );
+-----Tabela DrejtoriEkzekutiv------
+
+-----Tabela Selia------
+Create Table Selia(
+	Nr_Identifikues int Primary Key,
+	Emri varchar(20) Not Null,
+	Mbimeri varchar(20) Not Null,
+	Qyteti varchar(25) Not Null,
+	Rruga varchar(50) Not Null,
+	ZipKodi int Not Null,
+	Drejtori int Foreign Key References DrejtoriEkzekutiv(Id_Drejtori) On Update Cascade ON Delete Set Null
+);
+-----Tabela Selia------
