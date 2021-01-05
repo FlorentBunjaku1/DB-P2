b12e37fdca44a332632203ee9cc3b9692762c7bb
FlorentBunjaku1 <fb48234@ubt-uni.net>
2021-01-05 01:10:38 +0100

    Krijimi i Tableave : Drejtori, Selia, Vetura, Zyra, StafiPuntoreve

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,18 +4,6 @@ Use Projekti_OJQ
 -------------------------
 
 
------Tabela DrejtoriEkzekutiv------
-Create Table DrejtoriEkzekutiv(
-	Id_Drejtori int Primary Key 
-	Emri varchar(20) Not Null,
-	Mbimeri varchar(20) Not Null,
-	Qyteti varchar(25) Not Null,
-	Rruga varchar(50) Not Null,
-	ZipKodi int Not Null,
-	Zyrja int Foreign Key Zyrja(Nr_Dhomes) ON Update Cascade ON Delete Set Null
-);
------Tabela DrejtoriEkzekutiv------
-
 -----Tabela Selia------
 Create Table Selia(
 	Nr_Identifikues int Primary Key,
@@ -24,11 +12,54 @@ Create Table Selia(
 	Qyteti varchar(25) Not Null,
 	Rruga varchar(50) Not Null,
 	ZipKodi int Not Null,
-	Drejtori int Foreign Key References DrejtoriEkzekutiv(Id_Drejtori) On Update Cascade ON Delete Set Null
 );
 -----Tabela Selia------
 
+-----Tabela Vetura------
+Create Table Vetura(
+	Nr_Targave int Primary Key,
+	Lloji varchar(15),
+	NumriUlseve smallint,
+	DataServisimit date,
+	VitiProdhimit int,
+	Selia int Foreign Key References Selia(Nr_Identifikues) ON Update Cascade ON Delete Set Null
+);
+-----Tabela Vetura------
 
+
+-----Tabela StafiPuntorve------
+Create Table StafiPuntorve(
+	ID_Puntori int Primary Key,
+	Emri varchar(20) Not Null,
+	Mbimeri varchar(20) Not Null,
+	DateLindja Date,
+	Kualifikimi varchar(50),
+	Vetura int Foreign Key References Vetura(Nr_Targave) ON Update Cascade ON Delete Set Null
+);
+-----Tabela StafiPuntorve------
+
+-----Tabela Zyrja------
+Create Table Zyrja(
+	Nr_Dhomes int Primary Key Identity(1,1),
+	Kategoria varchar(30) Not Null,
+	Madhesia Float Default 90.5 ,
+	Puntori int Foreign Key References StafiPuntorve(ID_Puntori) ON Update Cascade ON Delete Set Null UNIQUE,
+	Selia int Foreign Key References Selia(Nr_Identifikues) 
+);
+-----Tabela Zyrja------
+
+-----Tabela DrejtoriEkzekutiv------
+Create Table DrejtoriEkzekutiv(
+	Id_Drejtori int Primary Key ,
+	Emri varchar(20) Not Null,
+	Mbimeri varchar(20) Not Null,
+	Qyteti varchar(25) Not Null,
+	Rruga varchar(50) Not Null,
+	ZipKodi int Not Null,
+	Zyrja int Foreign Key References Zyrja(Nr_Dhomes) ON Update Cascade ON Delete Set Null Unique,
+	Selia int Foreign Key References Selia(Nr_Identifikues) UNIQUE
+);
+-----Tabela DrejtoriEkzekutiv------
 
 ----Tabela Vizitori-----
 Create Table Vizitori(
